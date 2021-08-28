d8ddf8e3919171e64f7ea5c7d55a3f7af04f1e28
Marcolio97 <88984924+Marcolio97@users.noreply.github.com>
2021-08-28 16:45:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
 }
-function updateRate() 
+function onchange() 
 {
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval;
@@ -19,4 +18,3 @@ function updateRate()
      rate=3.5
      amount=1750000
      year=2025
-
