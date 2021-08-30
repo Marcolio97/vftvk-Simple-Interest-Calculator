0e1b05c6c853e354ed570f3410eaf4d1d85ef801
Marcolio97 <88984924+Marcolio97@users.noreply.github.com>
2021-08-30 11:32:23 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 function compute()
 {
-    var principal = Number(document.getElementById("principal").value);
-    var rate = document.getElementById("rate").value;
-    var years = document.getElementById("years").value;
-      var interest = principal * years * rate /100;
-     var year = new Date().getFullYear()+parseInt(years);
-     var amount = principal * years * rate /100;
-    document.getElementById("result").innerHTML="If you deposit <mark>"+principal+"</mark>,\<br\>at an interest rate of <mark>"+rate+"%</mark>\<br\>You will receive an amount of <mark>"+amount+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>"
+    if(principal.value < 1){
+        alert("Amount has to be a positive quantity")
+        principal.focus()
+        return
+    }
+    let interest = principal.value * years.value * rate.value / 100;
+    yearInFuture = new Date().getFullYear() + parseInt(years.value);
+    result = document.getElementById("result")
+    result.innerHTML = "<p>If you deposit <mark>"+ principal.value +"</mark>,<br>an interest rate of <mark>" + rate.value + "%</mark>.<br>You will receive an amount of <mark>"+interest+"</mark>,<br>in the year <mark>"+yearInFuture+"</mark></p>";
 }
 function updateRate() 
 {
