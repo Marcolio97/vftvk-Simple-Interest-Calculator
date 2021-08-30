5fee72681847a2b1cf1b082082da5419b753e2a4
Marcolio97 <88984924+Marcolio97@users.noreply.github.com>
2021-08-30 10:00:11 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
 function compute()
 {
-    var principal = document.getElementById("principal").value;
+    var principal = Number(document.getElementById("principal").value);
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
       var interest = principal * years * rate /100;
      var year = new Date().getFullYear()+parseInt(years);
-     console.log("hi")
+     var amount = principal * years * rate /100;
+    document.getElementById("result").innerHTML="If you deposit <mark>"+principal+"</mark>,\<br\>at an interest rate of <mark>"+rate+"%</mark>\<br\>You will receive an amount of <mark>"+amount+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>"
 }
 function updateRate() 
 {
     var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText=rateval;
+    document.getElementById("rate_val").innerText=rateval+"%";
 }
 
  function onclick()
-        document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
+ {
+        document.getElementById("result").innerHTML="If you deposit <mark>"+principal+"</mark>,\<br\>at an interest rate of <mark>"+rate+"%</mark>\<br\>You will receive an amount of <mark>"+amount+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>"
+    
+}
+
+function validation()
+{
+    var principal = document.getElementById("principal").value;
+    if (principal == NaN || principal <=0){
+        alert("Please enter a positive value!");
+        document.getElementById("principal").focus();
+    }
+    else {
+        compute();
+    }
+}
