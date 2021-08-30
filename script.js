6e45ba5daefb00137794e70f178a1a53bc8e12a2
Marcolio97 <88984924+Marcolio97@users.noreply.github.com>
2021-08-30 11:48:07 -0400

    Update script.js

--- script.js
+++ script.js
@@ -16,12 +16,6 @@ function updateRate()
     document.getElementById("rate_val").innerText=rateval+"%";
 }
 
- function onclick()
- {
-        document.getElementById("result").innerHTML="If you deposit <mark>"+principal+"</mark>,\<br\>at an interest rate of <mark>"+rate+"%</mark>\<br\>You will receive an amount of <mark>"+amount+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>"
-    
-}
-
 function validation()
 {
     var principal = document.getElementById("principal").value;
@@ -31,17 +25,5 @@ function validation()
     }
     else {
         compute();
-    }
-    function allowModals(){
-  for (const i of document.getElementsByTagName('iframe')) {
-    if (!i.sandbox.supports('allow-modals')) {
-      console.warn("Your browser doesn't support the 'allow-modals' attribute :(");
-      break;
-    }
-    if (i.sandbox.contains('allow-modals')) continue;
-    console.info(i, "doesn't allow modals");
-    i.sandbox.add('allow-modals');
-    console.info(i, 'now allows modals');
-  }
-}
+ 
 }
