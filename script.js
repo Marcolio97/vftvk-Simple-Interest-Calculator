f1e95bc7771e1723b9adb56ce460dbe19ae2d6cb
Marcolio97 <88984924+Marcolio97@users.noreply.github.com>
2021-08-31 09:15:30 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
 function compute()
 {
-    if(principal.value < 1){
-        alert("Amount has to be a positive quantity")
-        principal.focus()
-        return
-    }
-    let interest = principal.value * years.value * rate.value / 100;
-    yearInFuture = new Date().getFullYear() + parseInt(years.value);
-    result = document.getElementById("result")
-    result.innerHTML = "<p>If you deposit <mark>"+ principal.value +"</mark>,<br>an interest rate of <mark>" + rate.value + "%</mark>.<br>You will receive an amount of <mark>"+interest+"</mark>,<br>in the year <mark>"+yearInFuture+"</mark></p>";
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var year = new Date().getFullYear()+parseInt(years);
+    var interest = principal * years * rate /100;
+    document.getElementById("result").innerHTML="If you deposit <mark>" + principal + 
+        "</mark>, \<br\> at an interest rate of <mark>" + rate + "</mark>, \<br\> You will recieve an amount of <mark>" 
+        + interest + "</mark> \<br\> in the year <mark>" + year + "</mark> \<br\>";
+    
 }
 function updateRate() 
 {
     var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText=rateval+"%";
+    document.getElementById("rate_val").innerText=rateval + "%";
 }
-
 function validation()
 {
     var principal = document.getElementById("principal").value;
@@ -25,5 +24,5 @@ function validation()
     }
     else {
         compute();
- 
+    }
 }
