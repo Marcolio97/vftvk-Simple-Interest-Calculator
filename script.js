298b30741071eb2ea111a6cf500e31ed66756b8a
Marcolio97 <88984924+Marcolio97@users.noreply.github.com>
2021-08-31 09:45:02 -0400

    Update script.js

--- script.js
+++ script.js
@@ -7,14 +7,6 @@ function compute()
      var year = new Date().getFullYear()+parseInt(years);
      var amount = principal * years * rate /100;
     document.getElementById("result").innerHTML="If you deposit <mark>"+principal+"</mark>,\<br\>at an interest rate of <mark>"+rate+"%</mark>\<br\>You will receive an amount of <mark>"+amount+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>"
-    var principal = document.getElementById("principal").value;
-    if (principal == NaN || principal <=0);{
-        alert("Please enter a positive value!");
-        document.getElementById("principal").focus();
-    }
-    else {
-        compute();
-    }
 }
 function updateRate() 
 {
@@ -26,5 +18,16 @@ function updateRate()
  {
         document.getElementById("result").innerHTML="If you deposit <mark>"+principal+"</mark>,\<br\>at an interest rate of <mark>"+rate+"%</mark>\<br\>You will receive an amount of <mark>"+amount+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>"
     
-}   
+}
+
+function validation()
+{
+    var principal = document.getElementById("principal").value;
+    if (principal == NaN || principal <=0){
+        alert("Please enter a positive value!");
+        document.getElementById("principal").focus();
+    }
+    else {
+        compute();
+    }
 }
