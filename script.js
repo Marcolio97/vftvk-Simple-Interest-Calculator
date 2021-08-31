a20cb9dd72ec8ce1471ad123b832807981a1f5a0
Marcolio97 <88984924+Marcolio97@users.noreply.github.com>
2021-08-31 09:25:56 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
 function compute()
 {
-    var principal = document.getElementById("principal").value;
+    var principal = Number(document.getElementById("principal").value);
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
-    var year = new Date().getFullYear()+parseInt(years);
-    var interest = principal * years * rate /100;
-    document.getElementById("result").innerHTML="If you deposit <mark>" + principal + 
-        "</mark>, \<br\> at an interest rate of <mark>" + rate + "</mark>, \<br\> You will recieve an amount of <mark>" 
-        + interest + "</mark> \<br\> in the year <mark>" + year + "</mark> \<br\>";
-    
+      var interest = principal * years * rate /100;
+     var year = new Date().getFullYear()+parseInt(years);
+     var amount = principal * years * rate /100;
+    document.getElementById("result").innerHTML="If you deposit <mark>"+principal+"</mark>,\<br\>at an interest rate of <mark>"+rate+"%</mark>\<br\>You will receive an amount of <mark>"+amount+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>"
 }
 function updateRate() 
 {
     var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText=rateval + "%";
+    document.getElementById("rate_val").innerText=rateval+"%";
 }
+
+ function onclick()
+ {
+        document.getElementById("result").innerHTML="If you deposit <mark>"+principal+"</mark>,\<br\>at an interest rate of <mark>"+rate+"%</mark>\<br\>You will receive an amount of <mark>"+amount+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>"
+    
+}
+
 function validation()
 {
     var principal = document.getElementById("principal").value;
